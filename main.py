6c77021b87fe2b3432af71708f8ffb2125f35e05
asen <asen.borisov7@yahoo.com>
2022-11-26 08:41:46 +0200

    commit 1

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the The Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+psw = []
+for letter in range(nr_letters):
+    psw += random.choice(letters)
+for letter in range(nr_symbols):
+    psw += random.choice(symbols)
+for letter in range(nr_numbers):
+    psw += random.choice(numbers)
+
+random.shuffle(psw)
+
+password = ""
+for char in psw:
+    password += char
+
+print(f"Your password is:\n {password}")
+
